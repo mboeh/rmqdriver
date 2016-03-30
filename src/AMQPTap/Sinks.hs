c0d8678c6be1b72c212e5b2534384352e4b98a62
Matthew Boeh <m@mboeh.com>
2016-03-29 19:43:01 -0700

    Fix JSON sink and make it selectable.

--- src/AMQPTap/Sinks.hs
+++ src/AMQPTap/Sinks.hs
@@ -4,19 +4,26 @@ import AMQPTap.Types
 import AMQPTap.JSON (messageWithEnvelope)
 import qualified Text.JSON as JSON
 import Text.Printf (printf)
+import System.IO
 import qualified Network.AMQP as AM
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text as T
 
-defaultSink :: Sink
-defaultSink = SinkHandler handler
-  where handler = putStrLn . JSON.encodeStrict . messageWithEnvelope
-
+jsonSink :: Sink
+jsonSink = SinkHandler handler
+  where handler (msg, env) = do putStrLn . JSON.encodeStrict . messageWithEnvelope $ (msg, env)
+                                hFlush stdout
+textSink :: Sink
 textSink = SinkHandler handler
   where handler (msg, env) = 
           printf "[%s] %s\n" routingKey body
           where body       = BL.unpack $ AM.msgBody msg
                 routingKey = T.unpack $ AM.envRoutingKey env
 
+defaultSink :: Sink
+defaultSink = textSink
+
 mkSink :: String -> Sink
-mkSink _ = defaultSink
+mkSink "json" = jsonSink
+mkSink "text" = textSink
+mkSink _      = defaultSink

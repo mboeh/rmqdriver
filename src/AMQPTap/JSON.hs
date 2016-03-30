ad7644e5468f9b72da3004e15f377bc522852902
Matthew Boeh <m@mboeh.com>
2016-03-29 21:45:02 -0700

    Implemented more JSON fields.

--- src/AMQPTap/JSON.hs
+++ src/AMQPTap/JSON.hs
@@ -6,13 +6,38 @@ module AMQPTap.JSON ( message
 import Text.JSON
 import Network.AMQP 
 
+data MaybeNull a = NotNull a | Null
+
+nullable :: Maybe a -> MaybeNull a
+nullable (Just x) = NotNull x
+nullable Nothing  = Null
+
+instance (JSON a) => JSON (MaybeNull a) where
+  readJSON o = nullable <$> (readJSON o :: JSON a => Result (Maybe a))
+  showJSON (NotNull x) = showJSON x
+  showJSON Null        = JSNull
+
+optional :: JSON a => Maybe a -> JSValue
+optional = showJSON . nullable
+
 message :: Message -> JSValue
 message m = encJSDict [ ( "body", showJSON $ msgBody m )
+                      , ( "id", optional $ msgID m )
+                      , ( "type", optional $ msgType m )
+                      , ( "user_id", optional $ msgUserID m )
+                      , ( "application_id", optional $ msgApplicationID m )
+                      , ( "cluster_id", optional $ msgClusterID m )
+                      , ( "content_type", optional $ msgContentType m )
+                      , ( "content_encoding", optional $ msgContentEncoding m )
+                      , ( "reply_to", optional $ msgReplyTo m )
+                      , ( "correlation_id", optional $ msgCorrelationID m )
+                      , ( "expiration", optional $ msgExpiration m )
                       ]
 
 envelope :: Envelope -> JSValue
-envelope e = encJSDict [ ( "routingKey", envRoutingKey e )
-                       , ( "exchangeName", envExchangeName e )
+envelope e = encJSDict [ ( "routing_key", showJSON $ envRoutingKey e )
+                       , ( "exchange_name", showJSON $ envExchangeName e )
+                       , ( "redelivered", showJSON $ envRedelivered e )
                        ]
 
 messageWithEnvelope :: (Message, Envelope) -> JSValue

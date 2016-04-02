533d2431a3da09ced1719144cd66077dd95f2ff8
Matthew Boeh <m@mboeh.com>
2016-04-01 20:17:29 -0700

    Support for reading batch commands from stdin and command line.

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,23 @@ module Main where
 import AMQPTap
 import System.Environment (getArgs)
 import System.Exit (exitSuccess)
+import Data.List.Split (splitOneOf)
 import qualified System.Console.Readline as RL
 
-data Options = Options { amqpUri :: String
-                       , executeFile :: Maybe String
+data Options = Options { amqpUri       :: String
+                       , executeFile   :: Maybe String
+                       , executeScript :: Maybe String
                        }
 
 mkOptions :: Options
-mkOptions = Options "amqp://guest:guest@localhost" Nothing
+mkOptions = Options "amqp://guest:guest@localhost" Nothing Nothing
 
 parseOptions :: [String] -> Options
 parseOptions = (flip go) mkOptions
-  where go ("-u" : uri : rest) opts = go rest opts { amqpUri = uri }
-        go (filename : rest) opts   = go rest opts { executeFile = Just filename }
-        go _ opts                   = opts
+  where go ("-u" : uri : rest) opts    = go rest opts { amqpUri = uri }
+        go ("-e" : script : rest) opts = go rest opts { executeScript = Just script }
+        go (filename : rest) opts      = go rest opts { executeFile = Just filename }
+        go _ opts                      = opts
 
 -- Command interpreter
 
@@ -34,13 +37,14 @@ repl prompt state = do
 
 execScript :: EngineResult -> String -> IO ()
 execScript state text = do
-  let commands = (fmap parseCommand) . (fmap words) . lines $ text
+  let commands = (fmap parseCommand) . (fmap words) . (splitOneOf "\n;") $ text
   execCommands state commands
   where execCommands _ []           = return ()
         execCommands s (cmd : rest) = do newState <- execCommand s cmd
                                          execCommands newState rest
   
 execFile :: EngineResult -> String -> IO ()
+execFile state "-" = getContents >>= execScript state
 execFile state filename = readFile filename >>= execScript state
 
 main :: IO ()
@@ -48,6 +52,8 @@ main = do
   args <- getArgs
   let opts = parseOptions args
   engine <- connectEngine $ amqpUri opts 
-  case executeFile opts of
-    Nothing       -> repl "> " $ engineStartState engine
-    Just filename -> execFile (engineStartState engine) filename
+  case (executeFile opts, executeScript opts) of
+    (Nothing, Nothing) -> repl "> " $ engineStartState engine
+    (Just fn, Nothing) -> execFile (engineStartState engine) fn
+    (Nothing, Just sc) -> execScript (engineStartState engine) sc
+    _                  -> fail "Only one of -e or filename can be provided"
